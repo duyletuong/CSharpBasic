8cfe7041d96651835ddb90cd8d3ff559eaa13541
duyletuong <leduy27031997@gmail.com>
2026-07-05 10:38:15 +0700

    GetSelectedObjects, ContourPlate, Polybeam

--- MyApp/Form1.Designer.cs
+++ MyApp/Form1.Designer.cs
@@ -31,6 +31,12 @@
             this.btnSwaphandler = new System.Windows.Forms.Button();
             this.btnSelectObjects = new System.Windows.Forms.Button();
             this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.btnGetSelectedObjects = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreateContourPlate = new System.Windows.Forms.Button();
+            this.btnCreatePolybeam = new System.Windows.Forms.Button();
+            this.btnCreateBolt = new System.Windows.Forms.Button();
+            this.btnSetWorkPlaneToBEam = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSwaphandler
@@ -55,16 +61,81 @@
             // 
             // txtPrefix
             // 
-            this.txtPrefix.Location = new System.Drawing.Point(144, 86);
+            this.txtPrefix.Location = new System.Drawing.Point(215, 74);
             this.txtPrefix.Name = "txtPrefix";
             this.txtPrefix.Size = new System.Drawing.Size(125, 20);
             this.txtPrefix.TabIndex = 1;
+            // 
+            // btnGetSelectedObjects
+            // 
+            this.btnGetSelectedObjects.Location = new System.Drawing.Point(275, 13);
+            this.btnGetSelectedObjects.Name = "btnGetSelectedObjects";
+            this.btnGetSelectedObjects.Size = new System.Drawing.Size(125, 50);
+            this.btnGetSelectedObjects.TabIndex = 2;
+            this.btnGetSelectedObjects.Text = "Get selected Objects";
+            this.btnGetSelectedObjects.UseVisualStyleBackColor = true;
+            this.btnGetSelectedObjects.Click += new System.EventHandler(this.btnGetSelectedObjects_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Prefix";
+            // 
+            // btnCreateContourPlate
+            // 
+            this.btnCreateContourPlate.Location = new System.Drawing.Point(13, 129);
+            this.btnCreateContourPlate.Name = "btnCreateContourPlate";
+            this.btnCreateContourPlate.Size = new System.Drawing.Size(125, 27);
+            this.btnCreateContourPlate.TabIndex = 4;
+            this.btnCreateContourPlate.Text = "Create contour plate";
+            this.btnCreateContourPlate.UseVisualStyleBackColor = true;
+            this.btnCreateContourPlate.Click += new System.EventHandler(this.btnCreateContourPlate_Click);
+            // 
+            // btnCreatePolybeam
+            // 
+            this.btnCreatePolybeam.Location = new System.Drawing.Point(13, 162);
+            this.btnCreatePolybeam.Name = "btnCreatePolybeam";
+            this.btnCreatePolybeam.Size = new System.Drawing.Size(125, 27);
+            this.btnCreatePolybeam.TabIndex = 5;
+            this.btnCreatePolybeam.Text = "Create poly beam";
+            this.btnCreatePolybeam.UseVisualStyleBackColor = true;
+            this.btnCreatePolybeam.Click += new System.EventHandler(this.btnCreatePolybeam_Click);
+            // 
+            // btnCreateBolt
+            // 
+            this.btnCreateBolt.Location = new System.Drawing.Point(144, 129);
+            this.btnCreateBolt.Name = "btnCreateBolt";
+            this.btnCreateBolt.Size = new System.Drawing.Size(125, 27);
+            this.btnCreateBolt.TabIndex = 6;
+            this.btnCreateBolt.Text = "Create bolt";
+            this.btnCreateBolt.UseVisualStyleBackColor = true;
+            this.btnCreateBolt.Click += new System.EventHandler(this.btnCreateBolt_Click);
+            // 
+            // btnSetWorkPlaneToBEam
+            // 
+            this.btnSetWorkPlaneToBEam.Location = new System.Drawing.Point(13, 221);
+            this.btnSetWorkPlaneToBEam.Name = "btnSetWorkPlaneToBEam";
+            this.btnSetWorkPlaneToBEam.Size = new System.Drawing.Size(125, 46);
+            this.btnSetWorkPlaneToBEam.TabIndex = 7;
+            this.btnSetWorkPlaneToBEam.Text = "Set work plane to beam";
+            this.btnSetWorkPlaneToBEam.UseVisualStyleBackColor = true;
+            this.btnSetWorkPlaneToBEam.Click += new System.EventHandler(this.btnSetWorkPlaneToBEam_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(463, 279);
+            this.Controls.Add(this.btnSetWorkPlaneToBEam);
+            this.Controls.Add(this.btnCreateBolt);
+            this.Controls.Add(this.btnCreatePolybeam);
+            this.Controls.Add(this.btnCreateContourPlate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetSelectedObjects);
             this.Controls.Add(this.txtPrefix);
             this.Controls.Add(this.btnSelectObjects);
             this.Controls.Add(this.btnSwaphandler);
@@ -83,6 +154,12 @@
         private System.Windows.Forms.Button btnSwaphandler;
         private System.Windows.Forms.Button btnSelectObjects;
         private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.Button btnGetSelectedObjects;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateContourPlate;
+        private System.Windows.Forms.Button btnCreatePolybeam;
+        private System.Windows.Forms.Button btnCreateBolt;
+        private System.Windows.Forms.Button btnSetWorkPlaneToBEam;
     }
 }
 
--- MyApp/Form1.cs
+++ MyApp/Form1.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tekla.Structures.Model;
 using Tekla.Structures.Model.UI;
+using TSMUI = Tekla.Structures.Model.UI;
 using Tekla.Structures.Geometry3d;
+using System.Collections;
 
 namespace MyApp
 {
@@ -67,6 +69,111 @@ namespace MyApp
                 }
                 
             }
+        }
+
+        private void btnGetSelectedObjects_Click_1(object sender, EventArgs e)
+        {
+            TSMUI.ModelObjectSelector selector = new TSMUI.ModelObjectSelector();
+
+            ModelObjectEnumerator selectedObjects = selector.GetSelectedObjects();
+
+            while (selectedObjects.MoveNext())
+            {
+                if (selectedObjects.Current is Part)
+                {
+                    Part part = (Part)selectedObjects.Current;
+
+                    part.PartNumber.Prefix = txtPrefix.Text + part.PartNumber.Prefix;
+                    part.AssemblyNumber.Prefix = txtPrefix.Text + part.AssemblyNumber.Prefix;
+
+                    part.Modify();
+                }
+            }
+        }
+
+        private void btnCreateContourPlate_Click(object sender, EventArgs e)
+        {
+            ContourPlate contourPlate = new ContourPlate();
+            contourPlate.Profile.ProfileString = "PL10";
+            contourPlate.Material.MaterialString = "S235JR";
+
+            Picker picker = new Picker();
+
+            //using System.Collections;
+            ArrayList listPoints = picker.PickPoints(Picker.PickPointEnum.PICK_POLYGON);
+
+            Contour contour = new Contour();
+
+            //thêm điểm/chamfer cho contour plate
+            foreach (Point point in listPoints) 
+            {
+                Chamfer chamfer = new Chamfer();
+                ContourPoint contourPoint = new ContourPoint(point, chamfer);
+
+                contour.AddContourPoint(contourPoint);
+            }
+
+            contourPlate.Contour = contour;
+            contourPlate.Insert();
+            new Model().CommitChanges();
+        }
+
+        private void btnCreatePolybeam_Click(object sender, EventArgs e)
+        {
+            PolyBeam polyBeam = new PolyBeam();
+            polyBeam.Profile.ProfileString = "D50";
+            polyBeam.Material.MaterialString = "S235JR";
+
+            Picker picker = new Picker();
+
+            //using System.Collections;
+            ArrayList listPoints = picker.PickPoints(Picker.PickPointEnum.PICK_POLYGON);
+
+            Contour contour = new Contour();
+
+            //thêm điểm/chamfer cho contour plate
+            foreach (Point point in listPoints)
+            {
+                Chamfer chamfer = new Chamfer();
+                ContourPoint contourPoint = new ContourPoint(point, chamfer);
+
+                contour.AddContourPoint(contourPoint);
+            }
+
+            polyBeam.Contour = contour;
+            polyBeam.Insert();
+
+            new Model().CommitChanges();
+        }
+
+        private void btnCreateBolt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSetWorkPlaneToBEam_Click(object sender, EventArgs e)
+        {
+            CoordinateSystem local = new CoordinateSystem();
+            local.Origin = new Point(5000, 5000, 0);
+            local.AxisX = new Vector(0, 0, 1);
+            local.AxisY = new Vector(-1, 0, 0);
+
+            Model model = new Model();
+            WorkPlaneHandler wpHandler = model.GetWorkPlaneHandler();
+
+            // lấy tọa độ hiện hành
+            TransformationPlane userTransformationPlane = wpHandler.GetCurrentTransformationPlane();
+
+            //set tọa độ mới
+            wpHandler.SetCurrentTransformationPlane(new TransformationPlane(local));
+
+            ////vẽ vời
+            ///
+
+            //trả về workplane của ngừời dùng
+            wpHandler.SetCurrentTransformationPlane(userTransformationPlane)
+
+            model.CommitChanges();
         }
     }
 }
